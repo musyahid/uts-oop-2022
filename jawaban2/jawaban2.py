1100e27e9d1e56d59bafa0c73d6db61f35c99b61
Musyahid Abror <musyahid2@gmail.com>
2022-11-14 17:15:05 +0700

    Add Jawaban nomer 2

---
+++ jawaban2/jawaban2.py
@@ -0,0 +1,9 @@
+# Mengambil input
+print('================================')
+print('PROGRAM QUIZ')
+print('================================')
+nama = input("masukkan nama : ")
+nim = input("masukkan nim : ")
+
+# Menampilkan output
+print ("Hello",nama,"NIM kamu adalah",nim)
